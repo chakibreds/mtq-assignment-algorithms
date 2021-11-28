63b5a70bf16affcfb1e6face8bf6200f1361e351
chakib <celhouiti@gmail.com>
2021-11-28 20:38:15 +0100

    app de visualisation

--- src/graphviz.py
+++ src/graphviz.py
@@ -2,8 +2,15 @@ import sys
 import json
 from pygraphviz import *
 
+import dash
+from dash import dcc
+from dash import html
+import dash_cytoscape as cyto
+from dash.dependencies import Input, Output
+import plotly.express as px
+
 def drawGraph(filename, output='output/graph.png'):
-    with open(filename, 'r') as f:
+    """ with open(filename, 'r') as f:
         data = json.load(f)
     
     G = AGraph(data)
@@ -23,6 +30,42 @@ def drawGraph(filename, output='output/graph.png'):
     G.layout(prog='neato')
     G.draw(output)
     print(f"Fichier '{output}' créer avec succée")
+ """
+    with open(filename, 'r') as f:
+        data = json.load(f)
+    app = dash.Dash(__name__)
+    
+    list_elements = []
+    for inst in data:
+        dict_i = {'data':{'id' : inst, 'label' : str(inst)}, 'style': {
+            'background-color': 'red',
+            'line-color': 'red'
+        }}
+        list_elements.append(dict_i)
+        for s in data[inst]:
+            dict_s = {'data':{'id' : s, 'label' : str(s)}, 'style': {
+            'background-color': 'blue',
+            'line-color': 'blue'
+        }}
+            dict_edge = {'data':{'source' : s, 'target' : inst}}
+            list_elements.append(dict_s)
+            list_elements.append(dict_edge)
+
+    app.layout = html.Div([
+        html.P("Dash Cytoscape:"),
+        cyto.Cytoscape(
+            id='cytoscape',
+            elements=list_elements
+                #{'data': {'id': 'ca', 'label': 'Canada'}}
+               
+            ,
+            layout={'name': 'breadthfirst'},
+            style={'width': '1920px', 'height': '1080px'}
+        )
+    ])
+
+    app.run_server(debug=True)
+    
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
